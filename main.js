8b3a23fb09846a930eed0a8f2925cfb475479df7
luiscarGit <lucafiro1997@gmail.com>
2023-03-01 15:16:25 -0500

    Commit 1.1: En este podemos ver fucionados todos los elementos del nav, menuMobil con boton hamburguesa, menulogin con boton email y detalles de futura compra con el boton de carrit

--- main.js
+++ main.js
@@ -6,24 +6,32 @@ const shopping_cart_button = document.querySelector('.navbar-shopping-cart');
 const product_detail_menu = document.querySelector('.product-detail');
 
 
-emailButton.addEventListener('click', interMenuD);
 
+
+emailButton.addEventListener('click', interMenuD);/*ESCUCHAR SI LA CONSTANTE MENU HACE CLICK*/
 function interMenuD(){
-
     emailMenuD.classList.toggle('inactive');
-}
-
+    product_detail_menu.classList.add('inactive');
+} /* Funcion que intercambia el estado entre visible y no visible con el apuntador a HTML */
 
 botonMenu.addEventListener('click', interMenuMobile);
-
 function interMenuMobile(){
-
     MenuMobile.classList.toggle('inactive');
-    
+    product_detail_menu.classList.add('inactive');
 }
 
 shopping_cart_button.addEventListener('click', interProdetailMenu);
-
 function interProdetailMenu(){
     product_detail_menu.classList.toggle('inactive');
+    emailMenuD.classList.add('inactive')
+    MenuMobile.classList.add('inactive')
+}
+
+document.addEventListener('click', anyclick);
+
+function anyclick(){
+    
+    if (Event.target.closest('product-detail')) return;
+    product_detail_menu.style.display = "none"
+    
 }
